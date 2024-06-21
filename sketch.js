b63d309d5299952a7d7ee952a6049edf0387a404
ProfCarlosE <quirinodias@prof.educacao.sp.gov.br>
2024-06-21 09:46:55 -0300

    Adicionando arquivos css e js

---
+++ sketch.js
@@ -0,0 +1,22 @@
+// fazer raquete do opoente 
+
+let bolinhaX = 300
+let bolinhaY = 200
+let diametro = 22
+let raio = diametro/2
+
+// Variaveis da velocidade da bolinha
+let velocidadeBolinhaX = 3
+let velocidadeBolinhaY = 3
+
+//Variaveis da minha raquete
+let xRaquete = 5
+let yRaquete = 150
+let raqueteComprimento = 10
+let raqueteAltura = 90
+
+let colidiu = false
+
+//variáveis do oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
